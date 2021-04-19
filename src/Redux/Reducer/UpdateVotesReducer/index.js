0ac5cc3b9867e386fb5519174fc83b6aa0d87468
Marat Grigoryan <marat.grigoryan@betconstruct.com>
2021-04-19 12:33:26 +0400

    changes

--- src/Redux/Reducer/UpdateVotesReducer/index.js
+++ src/Redux/Reducer/UpdateVotesReducer/index.js
@@ -1,7 +1,7 @@
 import { updateVotesApi } from '../../Api';
 import { addDataAction } from '../../Action';
 
-export const updateVotes = params => async dispatch =>  {
+export const updateVotes = params => async dispatch => {
   const data = await updateVotesApi(params);
   data && dispatch(addDataAction(data));
 };
